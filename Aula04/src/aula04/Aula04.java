18d902bce474a0957833bfcf63b8d102ccba0117
Kawyz <kawannacisz04@gmail.com>
2025-04-29 21:51:46 -0300

    +1 exercicio

---
+++ Aula04/src/aula04/Aula04.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula04;
+
+/**
+ *
+ * @author kakaw
+ */
+public class Aula04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("Bic", "Amarela", 0.4f);
+        c1.Status();
+        
+        Caneta c2 = new Caneta("Tilibra", "Azul", 1.0f);
+        c2.Status();
+    } 
+}
